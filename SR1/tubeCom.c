7e2e135aea26b4161473d189d5f654ada7055bfa
Brawnyh <mamadouleye2b@gmail.com>
2026-03-16 22:34:18 +0100

    cours tubes

--- SR1/tubeCom.c
+++ SR1/tubeCom.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 //exo1 CM tubes cosas que pueden salir en el CC
 //leer en el teclado ->read(0)
 int main(int argc,char *argv[]){
@@ -12,6 +13,7 @@ int main(int argc,char *argv[]){
         perror("échec creation tube");
     }
     int pid=fork();
+
 
     // Père = lecteur, Fils = écrivain
     if (pid == 0) {          
@@ -33,3 +35,43 @@ int main(int argc,char *argv[]){
 int main(){
     
 }
+
+
+int main(){
+    int tube[2];
+    char msg[]="Bonjour du pére";
+    char buf[64];
+    if (pipe(tube)==-1){ perror("pipe");exit(1);}
+
+    int pid=fork();
+    if (pid==-1){ perror("fork");exit 2;}
+    if(pid==0){ //fils lecteure
+        close(tube[1]);//ferme ecriture
+        read(tube[0],buf,sizeof(buf));
+        close(tube[0]);
+        printf("fils reçoit %s\n",buf);
+        close(tube[0]);
+        exit(0);
+    }else{ //pere ecrivain
+        close(tube[0]);
+        write(tube[1],msg,strlen(msg)+1);
+        close(tube[1]);
+        wait(NULL);
+    }
+
+
+    //Lecture et ecriture non bloquante
+    //par deafaut read sur un tube vide est bloqué
+    int flags=fcntl(tube[0],F_GETFL);
+    flags |= O_NONBLOCK;
+    fcntl(tube[0],F_SETFL,flags);
+    //maintenant si tube vide:
+    int n=read(tube[0],buf,sizeof(buf));
+    if (n==-1 && errno ==EAGAIN){
+        //podemos hacer aqui otra cosa si tubo vacio
+    }
+
+}
+
+
+
