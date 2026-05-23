b7e3a3e5aaeffa1ec4635e23310111dea588aebf
Brawnyh <mamadouleye2b@gmail.com>
2026-05-23 19:15:48 +0200

    serveur 1

--- SR1/Réseaux/Exemples de codes avec lAPI Socket/TP-TCP-jeu-ETU/TP-TCP-jeu-ETU/serveur1.c
+++ SR1/Réseaux/Exemples de codes avec lAPI Socket/TP-TCP-jeu-ETU/TP-TCP-jeu-ETU/serveur1.c
@@ -5,22 +5,27 @@
 /* =================================================================== */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <signal.h>
-#include <tresor.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "tresor.h"
 
 /* =================================================================== */
 /* FONCTION PRINCIPALE : SERVEUR ITERATIF                              */
 /* =================================================================== */
 int main(int argc, char **argv) {
+    setbuf(stdout, NULL); //remplace flush stdout,pr pouvoir voir les prints en serveur
     //création socket
     int sock=socket(AF_INET,SOCK_STREAM,0);
     if (sock==-1){
         perror("erreur socket");
-        exit(0);
+        exit(1);
     }
     //On associe une addresse socket/numero service
     struct sockaddr_in local_addr,client_addr;
@@ -34,15 +39,35 @@ int main(int argc, char **argv) {
     listen(sock,5);
 
     //creation socket client dediée a cette connexion
-    int localAdressLongueur=sizeof(client_addr);
+    socklen_t localAdressLongueur=sizeof(client_addr);
     while(1){
-        int client_socket=accept(socket,(struct sockaddr*)&client_addr,&localAdressLongueur);
+        int client_socket=accept(sock,(struct sockaddr*)&client_addr,&localAdressLongueur);
         char buff[16];
-        recv(socket,buff,16,0);
-        int res;
-        sscanf(buff,"%d",res);
+        int lig,col,res;
+        do{
+            memset(buff,0,sizeof(buff));
+            recv(client_socket,buff,16,0);
+        
+            sscanf(buff,"%d %d",&lig,&col);
+            printf("cout reçu: %d %d",lig,col);
+            res=recherche_tresor(10,4,5,lig,col); //taille tableau,x_tresor,y_tresor
+    
+            printf("trouvé: %d",res);
+            sprintf(buff, "%d", res);
+            send(client_socket, buff, strlen(buff)+1, 0);
+            
+
+    }while(res!=0);
+    close(client_socket);
+
     }
 
+    
+
+
+    //fermeture
+    shutdown(sock,2);
+    close(sock);
 
     return 0;
 } // end main
